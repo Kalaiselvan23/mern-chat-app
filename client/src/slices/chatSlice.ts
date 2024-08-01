6da712c5e659e7f230a3b52060e634ad99aab82f
kalaiselvan23 <kalaiselvan1623@gmail.com>
2024-08-01 17:36:29 +0530

    added contacts

--- client/src/slices/chatSlice.ts
+++ client/src/slices/chatSlice.ts
@@ -1,30 +1,77 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState={
-    selectedChatType:undefined,
-    selectedChatData:undefined,
-    selectedChatMessages:[]
+interface ChatMessage {
+  sender: string | { _id: string };
+  recipient: string | { _id: string };
+  content: string;
+  messageType: string;
+  fileUrl?: string;
 }
-export const chatSlice=createSlice({
-    name:"chat",
-    initialState,
-    reducers:{
-        setSelectedChatType:(state,action)=>{
-            state.selectedChatType=action.payload
+
+interface ChatState {
+  selectedChatType: "contact" | "channel" | undefined;
+  selectedChatData: any;
+  selectedChatMessages: ChatMessage[];
+  directMessageContacts:any[],
+}
+
+const initialState: ChatState = {
+  selectedChatType: undefined,
+  selectedChatData: undefined,
+  selectedChatMessages: [],
+  directMessageContacts:[],
+};
+
+export const chatSlice = createSlice({
+  name: "chat",
+  initialState,
+  reducers: {
+    setSelectedChatType: (state, action: PayloadAction<"contact" | "channel" | undefined>) => {
+      state.selectedChatType = action.payload;
+    },
+    setSelectedChatData: (state, action: PayloadAction<any>) => {
+      state.selectedChatData = action.payload;
+    },
+    setSelectedMessages: (state, action: PayloadAction<ChatMessage[]>) => {
+      state.selectedChatMessages = action.payload;
+    },
+    closeChat: (state) => {
+      state.selectedChatType = undefined;
+      state.selectedChatData = undefined;
+    },
+    addMessage: (state, action: PayloadAction<ChatMessage>) => {
+      const message = action.payload;
+      console.log(`message from addMessage redux`)
+      const { selectedChatMessages, selectedChatType } = state;
+
+      state.selectedChatMessages = [
+        ...selectedChatMessages,
+        {
+          ...message,
+          recipient:
+            selectedChatType === "channel"
+              ? message.recipient
+              : message.recipient?._id,
+          sender:
+            selectedChatType === "channel"
+              ? message.sender
+              : message.sender?._id,
         },
-        setSelectedChatData:(state,action)=>{
-            state.selectedChatData=action.payload
-        },
-        setSelectedMessages:(state,action)=>{
-            state.selectedChatMessages=action.payload;
-        },
-        closeChat:(state)=>{
-            state.selectedChatType=undefined
-            state.selectedChatData=undefined
-        }
+      ];
+    },
+    setDirectMessageContacts:(state,action)=>{
+      state.directMessageContacts=action.payload
     }
+  },
+});
 
-})
+export const {
+  setSelectedChatType,
+  setSelectedChatData,
+  setSelectedMessages,
+  setDirectMessageContacts,
+  closeChat,
+  addMessage,
+} = chatSlice.actions;
 
-export const {setSelectedChatType,setSelectedChatData,setSelectedMessages,closeChat}=chatSlice.actions;
-export default chatSlice.reducer
+export default chatSlice.reducer;

e2354ddcc0851216ec6a7dff3ac88a941b832935
kalaiselvan23 <kalaiselvan1623@gmail.com>
2024-08-01 17:35:46 +0530

    user dropdown fixed

--- client/src/components/chats/UserDropDown.tsx
+++ client/src/components/chats/UserDropDown.tsx
@@ -7,9 +7,31 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { Button } from "../ui/button"
+import { LOGOUT_ROUTE } from "@/lib/constants"
+import apiClient from "@/lib/apiClient"
+import { toast } from "sonner"
 
 const UserDropDown = () => {
+    const navigate = useNavigate()
+    const handleLogout = async () => {
+        try {
+            const res = await apiClient.post(LOGOUT_ROUTE, {})
+            if (res.status === 200) {
+                toast("Successfully logged out")
+                navigate("/auth")
+            }
+            else{
+                toast.error("Unable to logout")
+            }
+
+        }
+
+        catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
@@ -25,9 +47,9 @@ const UserDropDown = () => {
                     <Link to={"/profile"}>Profile</Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
-                    <Link to={"/logout"}>
+                    <Button onClick={handleLogout}>
                         Logout
-                    </Link>
+                    </Button>
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
